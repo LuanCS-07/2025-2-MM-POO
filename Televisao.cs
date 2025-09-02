3532c57672ddaa0994fecc5d1d767bafa8ba3d7a
Luan Conrado <luanconrado07@gmail.com>
2025-09-02 00:38:13 +0000

    Feat: If and Else

--- Televisao.cs
+++ Televisao.cs
@@ -12,12 +12,18 @@ public class Televisao{
 
 public void AumentarVolume()
 {
-    Volume = Volume + 1;
+    if (Volume < 100)
+        Volume = Volume + 1;
+    else
+        Console.WriteLine(" TV ja esta no max."); 
 }
 
 public void DiminuirVolume()
 {
+    if (Volume > 0)
     Volume = Volume - 1;
+    else 
+        Console.WriteLine("TV ja esta no min.");
 }
 
 public void AumentarCanal()

f5426053f52fb17d95e5b1c55ab85b0dc1fd066b
Luan Conrado <luanconrado07@gmail.com>
2025-09-09 00:17:00 +0000

    Feat: Checkpoint I

--- Program.cs
+++ Program.cs
@@ -5,5 +5,24 @@ Televisao tv = new Televisao(55f);
 
 Console.WriteLine ($"A tv tem o tamanho {tv.Tamanho}");
 
+tv.LigarTelevisao();
+tv.AumentarCanal();
+
+tv.EscolherCanal(100);
+
+tv.DiminuirCanal();
+tv.DesligarTelevisao();
+
 tv.AumentarVolume();
 Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.Mudo();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.LigarTelevisao();

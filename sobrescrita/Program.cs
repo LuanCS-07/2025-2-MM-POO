1683b3ea122f5b5aa9c82ea8948169aa6fbc7754
Luan Conrado <luanconrado07@gmail.com>
2025-09-30 00:29:59 +0000

    Feat: SOBRESCRITA

---
+++ sobrescrita/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+Cartao cartaoBase = new Cartao();
+cartaoBase.Debitar(100);
+
+Console.WriteLine("-- CartaoDebito --");
+
+Cartao cartaoDebito = new Cartaodebito();
+cartaoDebito.Debitar(100);
